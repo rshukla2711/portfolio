298199fe453bf83628d61deb6a6273f065c40836
Roshan Kumar Shukla <btech10189.21@bitmesra.ac.in>
2023-07-22 23:44:31 +0530

    Fourth Commit

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
 window.onscroll = function() {myFunction()};
-var navbar = document.getElementById("navbar");
+var navbar = document.querySelector(".navbar");
 var sticky = navbar.offsetTop;
 function myFunction() {
-  if (window.scrollY >= sticky) {
+  if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
