fe6e2bfe58248110529b3b1ec33f77d61331f0c1
Roshan Kumar Shukla <btech10189.21@bitmesra.ac.in>
2023-07-23 16:02:12 +0530

    Fifth Commit

--- app.js
+++ app.js
@@ -25,3 +25,9 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+const resizeOps = () => {
+  document.documentElement.style.setProperty("--vh", window.innerHeight * 0.01 + "px");
+};
+
+resizeOps();
+window.addEventListener("resize", resizeOps);
